3f65fc55b69fa6306c088f674c36562533faa764
Agrell <Christian.Agrell@dnv.com>
2022-01-07 13:52:18 +0100

    tests for gp_diagnostics

---
+++ tests/test_rapid_models_gp_diagnostics_metrics.py
@@ -0,0 +1,43 @@
+import pytest 
+import numpy as np
+from scipy import stats 
+
+from rapid_models.gp_diagnostics.metrics import *
+
+def test_log_prob_normal():
+    """
+    test that log_prob_normal agrees with scipy.stats
+    """
+
+    # Generate some data
+    np.random.seed(12)
+    N = 17  
+
+    C = np.random.uniform(size = (N, N))
+    C = C.dot(C.T)
+    Y = np.random.multivariate_normal(mean = np.zeros(N), cov = C)
+
+    L = np.linalg.cholesky(C) 
+
+    # Compute likelihood 
+    loglik = log_prob_normal(L, Y)
+    loglik_scipy = stats.multivariate_normal.logpdf(Y, mean = np.zeros(N), cov = C)
+    
+    assert np.allclose(loglik, loglik_scipy)
+
+def test_log_prob_standard_normal():
+    """
+    test that log_prob_standard_normal agrees with scipy.stats
+    """
+
+    # Generate some data
+    np.random.seed(12)
+    N = 16  
+
+    Y = np.random.multivariate_normal(mean = np.zeros(N), cov = np.eye(N))
+
+    # Compute likelihood 
+    loglik = log_prob_standard_normal(Y)
+    loglik_scipy = stats.multivariate_normal.logpdf(Y, mean = np.zeros(N), cov = np.eye(N))
+    
+    assert np.allclose(loglik, loglik_scipy)
---
+++ tests/test_rapid_models_gp_diagnostics_utils_checks.py
@@ -0,0 +1,60 @@
+import pytest 
+
+from rapid_models.gp_diagnostics.utils.checks import *
+
+def test_is_numeric_np_array():
+    """
+    Test the function is_numeric_np_array() 
+    """
+
+    # These should be ok 
+    assert is_numeric_np_array(np.array([1, 2, 2.3]))
+    assert is_numeric_np_array(np.array([[1, 2, 2.3], [0, 1e-6, 0.001]]))
+    assert is_numeric_np_array(np.array(200))
+
+    # These are not ok
+    assert is_numeric_np_array(np.array([[1, 2, 2.3], [0, 0.001]])) == False
+    assert is_numeric_np_array(np.array(None)) == False
+    assert is_numeric_np_array(np.array([[1, 2, 2.3], [0, 0.001, 'a']])) == False
+    assert is_numeric_np_array('a') == False
+    assert is_numeric_np_array(50) == False
+    assert is_numeric_np_array([1, 2, 3]) == False
+
+def test_is_square():
+    """
+    Test the function is_square() 
+    """
+    # These should be ok 
+    assert is_square(np.ones(shape = (1, 1)))
+    assert is_square(np.ones(shape = (14, 14)))
+
+    # These are not ok
+    assert is_square(np.ones(shape = (13, 14))) == False 
+    assert is_square(np.ones(shape = (3, 2))) == False 
+    assert is_square(np.ones(shape = (3, 3, 3))) == False 
+    assert is_square(np.array(2)) == False 
+    assert is_square(np.array([1, 2, 3])) == False 
+
+def test_is_lower_triang():
+    """
+    Test the function is_lower_triang() 
+    """
+    # These should be ok 
+    arr = np.array([[0.2, 0, 0], 
+                    [3, 2.2, 0], 
+                    [1, 2, 4]])
+
+    assert is_lower_triang(arr)
+
+    arr = np.array([[1, 0], [2, 2.2]])
+    assert is_lower_triang(arr)
+    
+    arr = np.array([[1]])
+    assert is_lower_triang(arr)
+
+     # These are not ok
+    arr = np.array([[1, 2, 2.3], 
+                [0, 2.2, 3], 
+                [0.1, 0, 4]])
+
+    assert is_lower_triang(arr) == False 
