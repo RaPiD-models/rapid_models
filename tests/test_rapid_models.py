27f93dba73e7473e82a38d40ee8b3c581dd22942
Eldevik <Simen.Eldevik@dnv.com>
2022-01-11 15:47:15 +0100

    removed cli

--- tests/test_rapid_models.py
+++ tests/test_rapid_models.py
@@ -4,11 +4,6 @@
 
 import pytest
 
-from click.testing import CliRunner
-
-# from rapid_models import rapid_models
-from rapid_models import cli
-
 
 @pytest.fixture
 def response():
@@ -22,17 +17,3 @@ def response():
 def test_content(response):
     """Sample pytest test function with the pytest fixture as an argument."""
     assert response is None
-
-
-def test_command_line_interface():
-    """Test the CLI."""
-    runner = CliRunner()
-    result = runner.invoke(cli.main)
-    assert result.exit_code == 0
-    assert "rapid_models.cli.main" in result.output
-    help_result = runner.invoke(cli.main, ["--help"])
-    assert help_result.exit_code == 0
-    assert (
-        "--help" in help_result.output
-        and "Show this message and exit" in help_result.output
-    )
