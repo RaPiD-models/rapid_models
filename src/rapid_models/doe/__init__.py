d08b445115051f404d9cc2218022cbef8996aa8c
Eldevik <Simen.Eldevik@dnv.com>
2022-12-20 09:18:39 +0100

    added kmeans_sample to __init__

--- src/rapid_models/doe/__init__.py
+++ src/rapid_models/doe/__init__.py
@@ -1,5 +1,5 @@
 """Basic DOE package for rapid-models based on pyDOE2."""
 
-from .utils import fullfact_with_bounds, lhs_with_bounds, in_hull
+from .utils import fullfact_with_bounds, lhs_with_bounds, in_hull, kmeans_sample
 
-__all__ = ["fullfact_with_bounds", "lhs_with_bounds", "in_hull"]
+__all__ = ["fullfact_with_bounds", "lhs_with_bounds", "in_hull", "kmeans_sample"]
